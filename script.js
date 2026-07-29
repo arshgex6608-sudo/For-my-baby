4c03f26cb24322ff91940076eea4b7cdd7d78cb0
arshgex6608-sudo <arshgex6608@gmail.com>
2026-07-30 03:09:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ for (let i = 0; i < 150; i++) {
 
     document.body.appendChild(star);
 }
+// Floating hearts
+setInterval(() => {
+    const heart = document.createElement("div");
+
+    heart.innerHTML = "❤️";
+    heart.classList.add("heart");
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = (20 + Math.random() * 20) + "px";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+
+}, 500);

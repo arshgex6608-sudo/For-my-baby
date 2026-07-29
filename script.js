b361d618b9efdd6c03fe0da67b736ef74c64ae9c
arshgex6608-sudo <arshgex6608@gmail.com>
2026-07-30 03:03:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const button = document.querySelector("button");
 button.addEventListener("click", function () {
     alert("Coming soon... ❤️");
 });
+
+// Create stars
+for (let i = 0; i < 150; i++) {
+    const star = document.createElement("div");
+    star.classList.add("star");
+
+    star.style.left = Math.random() * 100 + "vw";
+    star.style.top = Math.random() * 100 + "vh";
+
+    star.style.animationDelay = Math.random() * 2 + "s";
+
+    document.body.appendChild(star);
+}
